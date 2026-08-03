f05a94681689a45ca57514538e045dad8dc68558
Aditya Kumar Parhi <adityakp2012@gmail.com>
2026-08-04 00:56:42 +0530

    fix: invert wheel index target math to align text data outcomes perfectly

--- Home/urls.py
+++ Home/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     # THE DOUBLE-ROUTE ALIAS TRICK: Catches both path requests safely!
     path('do-you-love-me/', views.love_query, name='love_query_old'),
     path('love-query/', views.love_query, name='love_query'),
-
     path('beginning/', views.beginning, name='beginning'),
     path('forever-vault/', views.forever_vault, name='forever_vault'),
     path('memories/', views.memories, name='memories'),
