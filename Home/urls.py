1c5e584fc2e0afdb5014be885907f6fda4f95f25
Aditya Kumar Parhi <adityakp2012@gmail.com>
2026-08-04 00:43:07 +0530

    fix: enforce dual-routing mapping aliases to completely catch do-you-love-me path requests

--- Home/urls.py
+++ Home/urls.py
@@ -3,9 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('love-query/', views.love_query, name='love_query'),  # MATCHED: Fixes the Homepage button error!
+
+    # THE DOUBLE-ROUTE ALIAS TRICK: Catches both path requests safely!
+    path('do-you-love-me/', views.love_query, name='love_query_old'),
+    path('love-query/', views.love_query, name='love_query'),
+
     path('beginning/', views.beginning, name='beginning'),
-    path('forever-vault/', views.forever_vault, name='forever_vault'), # SYNCHRONIZED
+    path('forever-vault/', views.forever_vault, name='forever_vault'),
     path('memories/', views.memories, name='memories'),
-    path('ending-page/', views.ending_page, name='ending_page'), # SYNCHRONIZED
+    path('ending-page/', views.ending_page, name='ending_page'),
 ]
